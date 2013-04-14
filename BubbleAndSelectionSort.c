9126b16bfaf3137ded55761da4bba71a695a737c
AdamRSterling <61sterling@cardinalmail.cua.edu>
2013-04-13 23:48:41 -0400

    first commit

--- BubbleAndSelectionSort.c
+++ BubbleAndSelectionSort.c
@@ -4,19 +4,19 @@ void bubbleSort(int *input)
 {
 	int temp;
 	int n = 10;
-	while (n > 0) {
+	while (n > 0) { 
 		int newn = 0;
 		for (int i = 1; i <= 9; i++) {
 
-			if (input[i-1] > input[i]) {
-				temp = input[i];
+			if (input[i-1] > input[i]) { //Step through the list, checking to see if each pair of items is sorted
+				temp = input[i]; //Swap the values if they're out of order
 				input[i] = input[i-1];
 				input[i-1] = temp;
 				newn = i;
 			}
 		}
 
-		n = newn;
+		n = newn; //If no items were swapped, then the list is sorted
 	}
 }
 
@@ -24,15 +24,15 @@ void selectionSort(int *input)
 {
 	int minIndex, temp;
 	for (int i = 0; i < 9; i++) {
-		minIndex = i;
+		minIndex = i; //Divide the array into an unsorted and a sorted part, where the sorted part is empty
 
 		for (int j = i + 1; j < 10; j++) {
-			if (input[j] < input[minIndex]) 
+			if (input[j] < input[minIndex]) //Step through the unsorted part of the array, looking for the smallest element
 				minIndex = j;
 		}
 			
-		if (minIndex != i) {
-			temp = input[i];
+		if (minIndex != i) { //If selection sort found an unsorted element, move it to the sorted array 
+			temp = input[i]; 
 			input[i] = input[minIndex];
 			input[minIndex] = temp; 
 		}
@@ -52,8 +52,8 @@ int main(int argc, char *argv[])
 		printf("%d ", input[i]);
 	}
 
+	//bubbleSort(input);
 	selectionSort(input);
-
 
 	printf("\nPost-sorted array: ");
 	for (int i = 0; i < 10; i++)
